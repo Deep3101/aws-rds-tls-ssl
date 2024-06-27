27458869351f4d96ef7c551b61b87db7f2f0e7c3
Deep-Inexture <deeppanchal@inexture.com>
2024-06-27 17:26:56 +0530

    Initial Commit

---
+++ src/main/java/com/springbootrdsdemo/SpringBootRdsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrdsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRdsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRdsDemoApplication.class, args);
+	}
+
+}
